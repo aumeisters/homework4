3ab78aba90db71f6fefac11484c4d4c7278ec149
aumeisters <54903608+aumeisters@users.noreply.github.com>
2019-09-08 17:53:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const executor = x => x();
 // справа нет вызова круглыми скобками, вместо () слева ф-ия обратного вызова
 executor(() => console.log('Меня вызвали!')); 
 
+
+//BONUS BONUS BONUS
+
+const curryPow = x => y => x ** y;
+const partiPow = x => y => y ** x;
+
+// const partiPow = ... // аналогично предыдущей функции
+console.log(curryPow(2)(10)); // 1024
+
+const qv = partiPow(2);
+const cb = partiPow(3);
+
+console.log(qv(5)); // 25
+console.log(cb(5)); // 125
